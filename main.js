5abebc01786ee77937721bc48cd1ec49a25520a0
Yitbarek Gitore <yitbareky@gmail.com>
2019-09-12 12:25:08 -0500

    intial commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var house = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
 let random_house = Math.floor(Math.random()*house.length);
 let studHouse = house[random_house];
-
+const fid = [];
+const studId = [];
 function myFunction()
 {
   var x = document.getElementById("formid");
@@ -14,15 +15,55 @@ function myFunction()
     x.style.display = "none";
   }
 }
-var printToDom = (divId, domString)=>{
-  document.getElementById(divId).innerHTML = domString;
+const printToDom = (divId, domString)=>{
+  document.getElementById(divId).innerHTML += domString;
 }
-var studName = document.getElementById('inlineFormInputGroup').value;
-var myfun = ()=>{
-  var studentContainer = `<div class="stud-main"><p>${studName}</p></div>`;
-  printToDom('stu-info',studentContainer);
-}
+// const expelButton = ()=>{    
+//   for (let i = 0; i < fid.length; i++) {
+//     const element = fid[i];
+//     if(element ===document.getElementById('sorting'))
+//     document.getElementById("stud-info-container").style.display = 'none';
+//   } 
+// }
+var i=0;
+const myfun = ()=>{  
+  var studName = document.getElementById('inlineFormInputGroup').value;
+    const studentContainer = `<div class="stud-container" id ="stud-info-container-{i}">
+        <h5>${studHouse}</h5>
+        <h5>${studName}</h5>
+        <a class = "remove-stud-info" href="#" value = "${i}" id="expelId">Expel</a>
+      </div>`
+    printToDom('stu-info',studentContainer);     
+    i++;
+};
 document.getElementById('sorting').addEventListener('click',myFunction);
-document.getElementById('sortingId').addEventListener('click',(e)=>{  
-  myfun();
+document.body.addEventListener('click', (event) => {
+  if (event.target.id === 'expelId') {
+      const buttonValue = event.target.value;
+      const cardId = `stud-info-container-${buttonValue}`;
+      const card = document.getElementById(cardId);
+      card.parentNode.removeChild(card);
+  }
+  if (event.target.id === 'sortingId') {
+      myfun();
+  }
+});
+
+document.getElementById('sortingId').addEventListener('click', (event) => {
+        event.preventDefault();
+});
+document.body.addEventListener('click', (event) => {
+  if (event.target.id === 'expelId') {
+      const buttonValue = event.target.value;
+      const cardId = `stud-info-container-${buttonValue}`;
+      const card = document.getElementById(cardId);
+      card.parentNode.removeChild(card);
+  }
+  if (event.target.id === 'sortingId') {
+      myfun();
+  }
+});
+
+document.getElementById('sortingId').addEventListener('click', (event) => {
+  event.preventDefault();
 });
